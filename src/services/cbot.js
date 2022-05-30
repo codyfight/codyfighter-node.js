c25081d8cd1d6c27e7e6c1910a08de722aaf213e
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2022-05-30 20:03:24 +0300

    logging

--- src/services/cbot.js
+++ src/services/cbot.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const GAME_MODE = 2; // random player
+// game settings
+const GAME_MODE = 2; // 1: custom player; 2 random player; 3 competitive arena
 const API_URL = 'https://game.codyfight.com';
 
-const GAME_STATUS_ENDED = 2;
+// default constants
+const GAME_STATUS_INIT = 0;
 const GAME_STATUS_PLAYING = 1;
-const GAME_STATUS_INITIALIZING = 0;
+const GAME_STATUS_TERMINATED = 2;
 
 export default class CBot {
 
@@ -20,21 +22,23 @@ export default class CBot {
         try {
             await this.play();
         } catch (e) {
-            console.error(`Runtime error! retrying...`, e);
+            console.error(`#### game failure ####\n* re-laucnhing the game...`, e);
             this.run();
         }
 
-        console.log('rematching...');
+        console.log('* re-laucnhing the game...');
         this.run();
     };
 
     play = async () => {
         // initialize a new game
         this.game = await this.init(this.CKey, GAME_MODE, null);
+        console.log('^^ game initialized', this.game.operators.bearer.name ? this.game.operators.bearer.name: '');
 
         // wait an opponent match
-        while (this.game.state.status === GAME_STATUS_INITIALIZING) {
+        while (this.game.state.status === GAME_STATUS_INIT) {
             this.game = await this.check(this.CKey);
+            console.log('++ Game state received');
         }
 
         // play the game
@@ -46,17 +50,20 @@ export default class CBot {
                 let y = this.game.operators.bearer.possible_moves[randomMove].y;
 
                 this.game = await this.move(this.CKey, x, y);
+                console.log('>> codyfighter move', x, y);
             } else {
                 this.game = await this.check(this.CKey);
+                console.log('++ game state received', this.game.state.status);
             }
         }
 
         // game ended
-        if (this.game.state.status === GAME_STATUS_ENDED) {
-            console.log('Game finished!', this.game.verdict);
+        if (this.game.state.status === GAME_STATUS_TERMINATED) {
+            console.log('!! game terminated !!', this.game.verdict);
         }
     };
 
+    // TODO: migrate to node.js client package
     init = async (CKey, mode, opponent) => {
         return await this.request('POST', { 'ckey': CKey, 'mode': mode, 'opponent': opponent });
     }
@@ -84,4 +91,5 @@ export default class CBot {
 
         return response.data;
     }
+    //
 };
